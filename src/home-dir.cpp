31708ee58d3ad5f0921155253a70376c735bfc1e
Evgeny Kislov <dev@evgenykislov.com>
2024-10-17 01:15:40 +0300

    Adds implementation for linux.

---
+++ src/home-dir.cpp
@@ -0,0 +1,71 @@
+/*
+MIT License
+
+Copyright (c) 2024 Evgeny Kislov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// Detect platform
+#if defined(linux) || defined(__linux) || defined(__linux__)
+#define LINUX_PLATFORM
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+#define WINDOWS_PLATFORM
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+#define MACOS_PLATFORM
+#else
+#error Unknown platform
+#endif
+
+#include "home-dir.h"
+
+// --------------------------
+// Linux implementation
+// --------------------------
+
+#ifdef LINUX_PLATFORM
+
+#include <string>
+
+#include <pwd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+const char kHomeEnvVar[] = "HOME";
+
+std::string HomeDirLibrary::GetHomeDirectory() {
+  try {
+    auto ev = getenv(kHomeEnvVar);
+    if (ev) {
+      return std::string(ev);
+    }
+    auto pw = getpwuid(getuid());
+    if (pw) {
+      return std::string(pw->pw_dir);
+    }
+  } catch (std::exception) {
+  }
+
+  return std::string();
+}
+
+#endif
+
+
--- src/home-dir.h
+++ src/home-dir.h
@@ -1,4 +1,36 @@
-#ifndef HOMEDIR_H
-#define HOMEDIR_H
+/*
+MIT License
 
-#endif // HOMEDIR_H
+Copyright (c) 2024 Evgeny Kislov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef HOMEDIR_H_EVGENY_KISLOV_20241017
+#define HOMEDIR_H_EVGENY_KISLOV_20241017
+
+#include <string>
+
+namespace HomeDirLibrary {
+
+  std::string GetHomeDirectory();
+
+}
+
+#endif // HOMEDIR_H_EVGENY_KISLOV_20241017
